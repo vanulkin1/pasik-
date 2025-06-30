3d5c3a8377991c146d35ce1846b9da5e4b75dea4
vanulkin1 <vanulkin@mail.ru>
2025-06-30 12:17:18 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+sessions_folder = "/VILOVSKIY BOTNET/sessions" # Путь к папке где лежат сессии
+
+admin = [426217102] # Айди админов
+
+api_token = "8064646276:AAFWkzy0cXEhMF3ZXLVXTkdNk0q5E7fhcIQ" # Токен бота
+
+Crypto_Pay_API_Token = "357259:AAIQ8eTVkkYcT5O2Ev1oJYniCHtFLKHhm6j" # Токен от криптобота
+
+log_chat_id = - # Айди лог чата с -100
+
+count = 18 # Количество жалоб с 1 сессии, не более 20
